45353bfc3c6740a065903ab9926142c15ce43a11
RobTyMitch <robertmitchell670@gmail.com>
2020-01-31 14:06:50 -0500

    added header
    PHP header

---
+++ PersonalProjectHeader.php
@@ -0,0 +1,35 @@
+<?php
+
+function printHeader($title){
+    ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="PersonalProjectCSS.css">
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+  <title> <?= $title ?> </title>
+</head>
+
+<body class="bgColor">
+  <div class="sidenav">
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>
+  <div class="container indexFont ">
+
+    <?php
+    $title = "Maintenance";
+}
+
+
+
+
