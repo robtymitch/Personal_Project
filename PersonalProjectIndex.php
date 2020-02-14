b1fe3769bb420372055ff10e2a0b8398c96f521e
RobTyMitch <robertmitchell670@gmail.com>
2020-02-14 15:03:05 -0500

    added functions

--- PersonalProjectIndex.php
+++ PersonalProjectIndex.php
@@ -14,7 +14,9 @@ printHeader($title);
             <h4 class="mt-0">' . $tenants[$i]['firstname'] . ' ' . $tenants[$i]['lastname'] . '</h4>
             <button type="button" class="btn btn-primary"><a href="PersonalProjectDetails.php?id=' . $i . '">View Work Order</a></button>
             <button type="button" class="btn btn-warning"><a href="modifyWorkOrder.php?id=' . $i . '">Modify Work Order</a></button>
-            <button type="button" class="btn btn-danger"><a href="deleteWorkOrder.php?id=' . $i . '">Delete Work Order</a></button>
+            <form method="post">
+              <input type="submit" name="del" id="del" value="Delete />
+            </form>
             <br />
             Invoice Date: ' . $tenants[$i]['date_of_issue'] . '
             </div>
@@ -26,13 +28,3 @@ printHeader($title);
     
     ?>
 
-
-  </div>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-
-</html>
