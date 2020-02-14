d549f80ccc2b41c3366d8fd12f9df26f78e4014b
RobTyMitch <robertmitchell670@gmail.com>
2020-02-14 12:28:48 -0500

    Put on buttons on tyhe work orders

--- PersonalProjectIndex.php
+++ PersonalProjectIndex.php
@@ -4,7 +4,6 @@ require('PersonalProjectHeader.php');
 printHeader($title);
 ?>
 
-
     <h1>Tenant Work Orders</h1>
     <br />
     <?php
@@ -13,7 +12,9 @@ printHeader($title);
             <img src="' . $tenants[$i]['picture'] . '" class="mr-3" alt="' . $tenants[$i]['firstname'] . ' ' . $tenants[$i]['lastname'] . '" width="96">
             <div class="media-body">
             <h4 class="mt-0">' . $tenants[$i]['firstname'] . ' ' . $tenants[$i]['lastname'] . '</h4>
-            <button type="button" class="btn btn-warning"><a href="PersonalProjectDetails.php?id=' . $i . '">View Work Order</a></button>
+            <button type="button" class="btn btn-primary"><a href="PersonalProjectDetails.php?id=' . $i . '">View Work Order</a></button>
+            <button type="button" class="btn btn-warning"><a href="modifyWorkOrder.php?id=' . $i . '">Modify Work Order</a></button>
+            <button type="button" class="btn btn-danger"><a href="deleteWorkOrder.php?id=' . $i . '">Delete Work Order</a></button>
             <br />
             Invoice Date: ' . $tenants[$i]['date_of_issue'] . '
             </div>
